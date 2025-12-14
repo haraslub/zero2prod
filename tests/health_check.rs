d1f0554dbc349297d1ce0b6d0b04d1e649f15a34
haraslub <lubos.harasta@gmail.com>
2025-12-14 23:09:32 +0100

    feat: add initial implementation of health check endpoint

---
+++ tests/health_check.rs
@@ -0,0 +1,29 @@
+use std::net::TcpListener;
+
+#[tokio::test]
+async fn healt_check_works() {
+    // Arrange
+    let address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // Act
+    let response = client
+        .get(&format!("{}/health_check", address))
+        .send()
+        .await
+        .expect("Failed to execute the request.");
+
+    // Assert
+    assert!(response.status().is_success());
+    assert_eq!(response.content_length(), Some(0));
+}
+
+fn spawn_app() -> String {
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind a random port");
+    let port = listener.local_addr().unwrap().port();
+    let server = zero2prod::run(listener).expect("Failed to bind the address");
+
+    let _ = tokio::spawn(server);
+
+    format!("http://127.0.0.1:{}", port)
+}
