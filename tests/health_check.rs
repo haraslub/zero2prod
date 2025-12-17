0a4de2793865bb783caefc9cd686c75102f9846e
haraslub <lubos.harasta@gmail.com>
2025-12-17 01:38:09 +0100

    feat: add configuration management and database connection settings

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,7 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-
 use zero2prod::startup::run;
+use zero2prod::configuration::get_configuration;
 
 /// Spin up an instance of our app
 /// and returns its address
@@ -14,8 +15,13 @@ fn spawn_app() -> String {
     format!("http://127.0.0.1:{}", port)
 }
 
-fn arrange_test() -> (String, reqwest::Client) {
+async fn arrange_test() -> (String, reqwest::Client) {
     let address = spawn_app();
+    let config = get_configuration().expect("Failed to read config.");
+    let connection_string = config.database_settings.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
     let client = reqwest::Client::new();
     (address, client)
 }
@@ -23,7 +29,7 @@ fn arrange_test() -> (String, reqwest::Client) {
 #[tokio::test]
 async fn health_check_works() {
     // Arrange
-    let (address, client) = arrange_test();
+    let (address, client) = arrange_test().await;
 
     // Act
     let response = client
@@ -40,7 +46,7 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_200_for_valid_form_data() {
     // Arrange
-    let (address, client) = arrange_test();
+    let (address, client) = arrange_test().await;
 
     // Act
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -59,7 +65,7 @@ async fn subscribe_returns_200_for_valid_form_data() {
 #[tokio::test]
 async fn subscribe_returns_400_when_data_is_missing() {
     // Arrange
-    let (address, client) = arrange_test();
+    let (address, client) = arrange_test().await;
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
